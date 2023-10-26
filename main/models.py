b6023d85d6b70ffea102702344da5059a8c0fc88
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2023-10-26 16:22:16 +0700

    add the review app

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Books(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     author = models.CharField(max_length=50, null=True, blank=True)
     rating = models.FloatField(null=True, blank=True)
-    review = models.IntegerField(null=True, blank=True)
+    num_review = models.IntegerField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     genre = models.TextField(null=True, blank=True)
--- review/models.py
+++ review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from main.models import Books
+# from main import Books
 
 # Create your models here.
+
+class Review(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    books = models.ForeignKey(Books, on_delete=models.CASCADE)
+    review_title = models.CharField(max_length=50)
+    review = models.TextField()
+    date_added = models.DateField()
+    rating_new = models.FloatField()
