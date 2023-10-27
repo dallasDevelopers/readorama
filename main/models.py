e110e1a10a2528cce2c130d74dc3654549cf5c63
AdamMoh <xlkartu12345@gmail.com>
2023-10-27 11:15:26 +0700

    load books data

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Books(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
-    author = models.CharField(max_length=50, null=True, blank=True)
+    name = models.TextField(null=True, blank=True)
+    author = models.TextField(null=True, blank=True)
     rating = models.FloatField(null=True, blank=True)
     num_review = models.IntegerField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
