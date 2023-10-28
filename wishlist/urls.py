af477c72cbbda21cb3ab548100a403c1e8dd381f
brmntyprytm <bramantyopriyou@gmail.com>
2023-10-28 17:49:08 +0700

    added remove a book from wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from wishlist.views import show_wishlist, add_wishlist
+from wishlist.views import show_wishlist, delete_wishlist, show_xml, show_json, show_xml_by_id, show_json_by_id, get_product_json, delete_product_ajax
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='wishlist_main'),
-    path('add', add_wishlist, name='add_wishlist')
+    path('delete/<int:wishlist_id>/', delete_wishlist, name='delete_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path("get-product/", get_product_json, name="get_product_json"),
+    path(
+        "delete-product-ajax/<int:id>", delete_product_ajax, name="delete_product_ajax"
+    ),
 ]
