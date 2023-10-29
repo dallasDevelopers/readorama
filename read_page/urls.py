aa0330ae6cdbe95bdd40d71e1ae9e4ac8db5b651
OmarK2804 <khalifomar4@gmail.com>
2023-10-29 15:49:32 +0700

    Add AJAX

--- read_page/urls.py
+++ read_page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from read_page.views import show_read_page, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_product
+from read_page.views import show_read_page, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_product, get_product_json
 
 app_name = 'read_page'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('delete/<int:id>', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
 ]
