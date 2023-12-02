b9d04f990369ba7ca3d7ed28cd5e43d07816784c
fiardiel <rafi.ardiel@ui.ac.id>
2023-12-02 17:36:56 +0700

    Added add book flutter for admin

--- landing_admin/views.py
+++ landing_admin/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from main.models import Books
@@ -7,6 +7,7 @@ from landing_admin.forms import BookForm
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
+import json
 
 
 from functools import wraps
@@ -114,3 +115,26 @@ def delete_ajax(request, id):
 def get_book_by_id(request, id):
     item = Books.objects.filter(pk=id)
     return HttpResponse(serializers.serialize('json', item))
+
+
+@csrf_exempt
+def add_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_book = Books.objects.create(
+            name = data["name"],
+            author = data["author"],
+            rating = float(data["rating"]),
+            num_review = int(data["num_review"]),
+            price = int(data["price"]),
+            year = int(data["year"]),
+            genre = data["genre"]
+        )
+
+        new_book.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
