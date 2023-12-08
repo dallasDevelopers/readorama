a81132de189bfdb5fef24ddb61d5e3a52e14f927
OmarK2804 <khalifomar4@gmail.com>
2023-12-08 14:33:57 +0700

    delete flutter

--- read_page/urls.py
+++ read_page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from read_page.views import show_read_page, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_product, get_product_json
+from read_page.views import show_read_page, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_product, get_product_json, delete_product_flutter
 
 app_name = 'read_page'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
+    path('delete-flutter/<int:id>', delete_product_flutter, name='delete_product_flutter'),
 ]
